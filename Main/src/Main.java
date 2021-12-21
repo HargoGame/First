19a694acad59ba3a31f059cf6f260b98085e7fc9
HargoGame <kopernik182@mail.ru>
2021-12-21 11:00:11 +0500

    5.6.6

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,49 +1,12 @@
 public class Main {
-
     public static void main(String[] args) {
-
+        System.out.println("git checkout -b имя_ветки");
+        System.out.println("git checkout имя_ветки");
+        System.out.println("git branch -m новое_название");
+        System.out.println("git branch -d");
 
     }
 }
-
-class Separator {
-    int[] array;
-
-    public Separator(int[] array) {
-        this.array = array;
-    }
-
-    int[] even() {
-        int k = 0, now = 0;
-        for (int i : array) {
-            if (i % 2 == 0) k++;
-        }
-        int[] evens = new int[k];
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 0) {
-                evens[now] = array[i];
-                now += 1;
-            }
-        }
-        return evens;
-    }
-
-    int[] odd() {
-        int k = 0, now = 0;
-        for (int i : array) {
-            if (i % 2 != 0) k++;
-        }
-        int[] odds = new int[k];
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 != 0) {
-                odds[now] = array[i];
-                now += 1;
-            }
-        }
-        return odds;
-    }
-}
-
 
 
 
