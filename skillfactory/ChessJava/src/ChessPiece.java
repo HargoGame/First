931ab5626fccb9cbaeae4852d6e2fed1cf23363a
HargoGame <kopernik182@mail.ru>
2022-01-26 09:44:48 +0500

    2

--- skillfactory/ChessJava/src/ChessPiece.java
+++ skillfactory/ChessJava/src/ChessPiece.java
@@ -1,4 +1,12 @@
 public abstract class ChessPiece {
     String color;
     boolean chek = true;
+
+    public ChessPiece(String color){
+        this.color = color;
+    }
+
+    public abstract String getColor();
+    public abstract boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
+    public abstract String getSymbol();
 }
