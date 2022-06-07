bccd5421dcb8686e5d434cb622b19c2710ac1674
HargoGame <kopernik182@mail.ru>
2022-06-07 10:00:40 +0500

    1

--- skillfactory/Greeting/src/main/java/Greeting.java
+++ skillfactory/Greeting/src/main/java/Greeting.java
@@ -1,7 +1,6 @@
-public class Main {
+public class Greeting {
 
     public static void main(String[] args) {
-
         System.out.println("Hello World");
     }
 }
---
+++ skillfactory/test/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        double d = 2.0;
+        double c = 1.1;
+
+        System.out.println(d - c);
+    }
+}
